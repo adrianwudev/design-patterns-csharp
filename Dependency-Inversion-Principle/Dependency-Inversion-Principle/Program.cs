3a59f26052ddff1dc7a4671ea26e9f6baf7731e3
adrian <adrian657984@gmail.com>
2022-03-26 23:25:33 +0800

    Dependency-Inversion-Principle

---
+++ Dependency-Inversion-Principle/Dependency-Inversion-Principle/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dependency_Inversion_Principle
+{
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+    }
+
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    public class Relationships : IRelationshipBrowser // low-level
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild (Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            return relations
+                .Where(x => x.Item1.Name == name 
+                    && x.Item2 == Relationship.Parent)
+                        .Select(r => r.Item3);
+        }
+    }
+    public class Research // high-level
+    {
+        public Research (IRelationshipBrowser browser)
+        {
+            foreach (var p in browser.FindAllChildrenOf("John"))
+            {
+                Console.WriteLine($"John has a child called {p.Name}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Matt" };
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
+        }
+    }
+}
