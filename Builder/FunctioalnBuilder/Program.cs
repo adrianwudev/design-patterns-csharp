88657d0b49f5d88a5dd76953be6d31d688455e87
adrian <adrian657984@gmail.com>
2022-04-04 18:21:58 +0800

    Functional-Builder

---
+++ Builder/FunctioalnBuilder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionalBuilder
+{
+    public class Person
+    {
+        public string Name, Position;
+
+        public override string ToString()
+        {
+            return $"{Name} is a {Position}";
+        }
+    }
+
+    public sealed class PersonBuilder
+    {
+        public readonly List<Action<Person>> Actions
+            = new List<Action<Person>>();
+
+        public PersonBuilder Called(string name)
+        {
+            Actions.Add(p => { p.Name = name; });
+            return this;
+        }
+
+        public Person Build()
+        {
+            var p = new Person();
+            Actions.ForEach(a => a(p));
+            return p;
+        }
+
+    }
+
+    public static class PersonBuilderExtensions
+    {
+        public static PersonBuilder WorkAsA
+            (this PersonBuilder builder, string position)
+        {
+            builder.Actions.Add(p =>
+            {
+                p.Position = position;
+            });
+            return builder;
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            var person = pb.Called("Damian")
+                .WorkAsA("Developer")
+                .Build();
+
+            Console.WriteLine(person);
+        }
+    }
+}
