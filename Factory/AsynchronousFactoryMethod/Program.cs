d50b700ac004f4094f41f04a6fb328017348d367
adrian <adrian657984@gmail.com>
2022-04-06 01:45:57 +0800

    Asynchronous-Factory-Method

---
+++ Factory/AsynchronousFactoryMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsynchronousFactoryMethod
+{
+    public class Foo
+    {
+        private string FooName;
+        private Foo()
+        {
+            FooName = "FooString";
+        }
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(FooName)}: {FooName}";
+        }
+    }
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var x = await Foo.CreateAsync();
+            Console.WriteLine(x);
+        }
+    }
+}
