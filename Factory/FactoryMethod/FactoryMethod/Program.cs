dd6be27a41022df7b308f2f14e51ecc163603405
adrian <adrian657984@gmail.com>
2022-04-05 23:35:37 +0800

    Factory-Method

---
+++ Factory/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Point
+    {
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        private double x, y;
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var CartesianPoint = Point.NewCartesianPoint(1.0, 1.0);
+            Console.WriteLine("CartesianPoint");
+            Console.WriteLine(CartesianPoint);
+
+            var PolarPoint = Point.NewPolarPoint(1.0, 1.0);
+            Console.WriteLine("\nPolarPoint");
+            Console.WriteLine(PolarPoint);
+        }
+    }
+}
