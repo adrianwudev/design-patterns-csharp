8142c44ba377ed47b3b3392118ad63790755f072
adrian <adrian657984@gmail.com>
2022-04-11 01:13:07 +0800

    Inner Factory

--- Factory/Factory/Program.cs
+++ Factory/Factory/Program.cs
@@ -8,31 +8,33 @@ namespace Factory
         Polar
     }
 
-    public static class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
-
     public class Point
     {
         private double x, y;
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
 
+        public static Point Origin2 = new Point(0, 0);
+
         public override string ToString()
         {
             return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
         }
     }
 
@@ -40,13 +42,17 @@ namespace Factory
     {
         static void Main(string[] args)
         {
-            var CartesianPoint = PointFactory.NewCartesianPoint(1.0, 1.0);
+            var CartesianPoint = Point.Factory.NewCartesianPoint(1.0, 1.0);
             Console.WriteLine("CartesianPoint");
             Console.WriteLine(CartesianPoint);
 
-            var PolarPoint = PointFactory.NewPolarPoint(1.0, 1.0);
+            var PolarPoint = Point.Factory.NewPolarPoint(1.0, 1.0);
             Console.WriteLine("\nPolarPoint");
             Console.WriteLine(PolarPoint);
+
+            var OriginPoint = Point.Origin2;
+            Console.WriteLine("\nOriginPoint");
+            Console.WriteLine(OriginPoint);
         }
     }
 }
