8bf5c1de9644940f87f3a29bd79bf37f6ed58375
adrian <adrian657984@gmail.com>
2022-04-18 02:28:59 +0800

    Prototype-Copy-Constructors

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CopyConstructor
+{
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names ?? throw new ArgumentNullException(nameof(names));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName));
+            HouseNumber = houseNumber;
+        }
+
+        public Address(Address other)
+        {
+            StreetName = other.StreetName;
+            HouseNumber = other.HouseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person(new[] { "John", "Smith" },
+                new Address("London Road", 456));
+
+            var jane = new Person(john);
+            jane.Address.HouseNumber = 123;
+            jane.Names = new[] { "Jane", "Smith" };
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+        }
+    }
+}
