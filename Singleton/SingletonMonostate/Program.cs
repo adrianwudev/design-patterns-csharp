3f2fdc272224a5d5370efaa883d87f2b47b96ee0
adrian <adrian657984@gmail.com>
2022-04-24 22:34:14 +0800

    Singleton-Monostate

---
+++ Singleton/SingletonMonostate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SingletonMonostate
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var CEO = new CEO();
+            CEO.Name = "John";
+            CEO.Age = 45;
+            Console.WriteLine(CEO);
+
+            var CEO2 = new CEO();
+            CEO.Age = 32;
+            Console.WriteLine(CEO2);
+        }
+    }
+}
