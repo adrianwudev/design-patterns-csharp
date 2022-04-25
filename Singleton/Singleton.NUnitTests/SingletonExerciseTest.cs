39928858728176ed926c35299db8ac54731b6193
adrian <adrian657984@gmail.com>
2022-04-26 01:25:26 +0800

    Singleton-Exercise-IsSingleton

---
+++ Singleton/Singleton.NUnitTests/SingletonExerciseTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SingletonExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.NUnitTests
+{
+    [TestFixture]
+    public class SingletonExerciseTest
+    {
+        [Test]
+        public void SingletonIsReferenceEqualTest()
+        {
+            var obj = new object();
+            Assert.IsTrue(SingletonTester.IsSingleton(() => obj));
+            Assert.IsFalse(SingletonTester.IsSingleton(() => new object()));
+        }
+    }
+}
---
+++ Singleton/SingletonExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SingletonExercise
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            // todo
+            var obj1 = func.Invoke();
+            var obj2 = func.Invoke();
+
+            // ReferenceEquals(obj1, obj2);
+            return obj1.Equals(obj2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
