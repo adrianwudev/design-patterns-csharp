fd95e08e5302c2d03d2540e82e555697202cf134
adrian <adrian657984@gmail.com>
2022-04-29 05:26:48 +0800

    Adapter-Generic-Value

---
+++ Adapter/AdapterGenericValue/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace AdapterGenericValue
+{
+    // Vector2f, Vector3i
+
+    public interface IInteger
+    {
+        int Value { get; }
+    }
+
+    public static class Dimensions
+    {
+        public class Two : IInteger
+        {
+            public int Value => 2;
+        }
+
+        public class Three : IInteger
+        {
+            public int Value => 3;
+        }
+    }
+
+    public class Vector<TSelf, T, D>
+        where D : IInteger, new()
+        where TSelf : Vector<TSelf, T, D>, new()
+    {
+        protected T[] data;
+
+        public Vector()
+        {
+            data = new T[new D().Value];
+        }
+
+        public Vector (params T[] values)
+        {
+            var requireSize = new D().Value;
+            data = new T[requireSize];
+
+            var providedSize = values.Length;
+
+            for (int i = 0; i < Math.Min(requireSize, providedSize); i++)
+                data[i] = values[i];
+        }
+
+        public static TSelf Create(params T[] values)
+        {
+            var result = new TSelf();
+
+            var requireSize = new D().Value;
+            result.data = new T[requireSize];
+
+            var providedSize = values.Length;
+
+            for (int i = 0; i < Math.Min(requireSize, providedSize); i++)
+                result.data[i] = values[i];
+
+            return result;
+        }
+
+        public T this[int index]
+        {
+            get => data[index];
+            set => data[index] = value;
+        }
+    }
+
+    public class VectorOfFloat<TSelf, D> 
+        : Vector<TSelf, float, D>
+        where D : IInteger, new()
+        where TSelf : Vector<TSelf, float, D>, new()
+    {
+
+    }
+
+    public class VectorOfInt<D> : Vector<VectorOfInt<D>, int, D>
+        where D : IInteger, new()
+    {
+        public VectorOfInt()
+        {
+        }
+
+        public VectorOfInt(params int[] values) : base(values)
+        {
+        }
+
+        public static VectorOfInt<D> operator +
+            (VectorOfInt<D> lhs, VectorOfInt<D> rhs)
+        {
+            var result = new VectorOfInt<D>();
+            var dim = new D().Value;
+            for (int i = 0; i < dim; i++)
+            {
+                result[i] = lhs[i] + rhs[i];
+            }
+
+            return result;
+        }
+    }
+
+    public class Vector2i : VectorOfInt<Dimensions.Two>
+    {
+        public Vector2i()
+        {
+        }
+
+        public Vector2i(params int[] values) : base(values)
+        {
+        }
+    }
+
+    public class Vecotr3f 
+        : VectorOfFloat<Vecotr3f, Dimensions.Three>
+    {
+        public override string ToString()
+        {
+            return $"{string.Join(",", data)}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new Vector2i(1, 2);
+            v[0] = 0;
+
+            var vv = new Vector2i(3, 2);
+            var result = v + vv;
+
+            Vecotr3f u = Vecotr3f.Create(3.5f, 2.2f, 1);
+        }
+    }
+}
