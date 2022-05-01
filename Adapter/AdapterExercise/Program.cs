2778f27ceca7c25c404a55a0070b0dd706c9574a
adrian <adrian657984@gmail.com>
2022-05-02 02:30:14 +0800

    Adapter-Exercise

---
+++ Adapter/AdapterExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AdapterExercise
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        private readonly Square _square;
+        public SquareToRectangleAdapter(Square square)
+        {
+            // todo
+            _square = square;
+        }
+
+        public int Width => _square.Side;
+        public int Height => _square.Side;
+        // todo
+
+    }
+}
