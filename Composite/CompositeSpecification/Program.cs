12034414b8df8dac4615c8db394467e0e7d53ea7
adrian <adrian657984@gmail.com>
2022-05-10 06:42:01 +0800

    Composite-Specification

---
+++ Composite/CompositeSpecification/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompositeSpecification
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        Small, Medium, Large, Huge
+    }
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Product(string name, Color color, Size size)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public abstract class ISpecification<T>
+    {
+        public abstract bool IsSatisfied(T p);
+
+        public static ISpecification<T> operator &(
+            ISpecification<T> first, ISpecification<T> second)
+        {
+            return new AndSpecification<T>(first, second);
+        }
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public override bool IsSatisfied(Product p)
+        {
+            return p.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+        public override bool IsSatisfied(Product p)
+        {
+            return p.Size == size;
+        }
+    }
+
+    public abstract class CompositeSpecification<T> : ISpecification<T>
+    {
+        protected readonly ISpecification<T>[] items;
+
+        public CompositeSpecification(params ISpecification<T>[] items)
+        {
+            this.items = items;
+        }
+    }
+
+    // combinator
+    public class AndSpecification<T> : CompositeSpecification<T>
+    {
+        public AndSpecification(params ISpecification<T>[] items) : base(items)
+        {
+        }
+
+        public override bool IsSatisfied(T t)
+        {
+            // Any => OrSpecification
+            return items.All(i => i.IsSatisfied(t));
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+            {
+                if (spec.IsSatisfied(i))
+                    yield return i;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                Console.WriteLine($" - {p.Name} is green");
+
+            Console.WriteLine("Blue products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Blue)))
+                Console.WriteLine($" - {p.Name} is blue");
+
+            Console.WriteLine("Large products (new):");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                Console.WriteLine($" - {p.Name} is large");
+
+            Console.WriteLine("Large blue items (new):");
+            foreach (var p in bf.Filter(products, new AndSpecification<Product>(new SizeSpecification(Size.Large), new ColorSpecification(Color.Blue))))
+                Console.WriteLine($" - {p.Name} is large and blue");
+        }
+    }
+}
