ad44c3d2e24e28001cc860f02672979755a79a51
adrian <adrian657984@gmail.com>
2022-05-11 07:43:31 +0800

    Composite-Exercise

---
+++ Composite/CompositeExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CompositeExercise
+{
+    public interface IValueContainer : IEnumerable<int>
+    {
+
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class ManyValues : List<int>, IValueContainer
+    {
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Sum(this List<IValueContainer> containers)
+        {
+            int result = 0;
+            foreach (var c in containers)
+                foreach (var i in c)
+                    result += i;
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleValue = new SingleValue { Value = 11 };
+            var otherValues = new ManyValues();
+            otherValues.Add(22);
+            otherValues.Add(33);
+
+            bool isSumUpCorrect = new List<IValueContainer> { singleValue, otherValues }.Sum().Equals(66);
+
+            Console.WriteLine(isSumUpCorrect);
+        }
+    }
+}
