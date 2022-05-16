5d394751d0f79f979db9f285df52093e7d74f0d8
adrian <adrian657984@gmail.com>
2022-05-17 03:30:24 +0800

    Decorator-Multiple-Inheritance-With-Default-Interface-Members

---
+++ Decorator/DecoratorMultipleInheritanceWithDefaultInterfaceMembers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DecoratorMultipleInheritanceWithDefaultInterfaceMembers
+{
+    using static System.Console;
+    public interface ICreature
+    {
+        int Age { get; set; }
+    }
+
+    public interface IBird : ICreature
+    {
+        void Fly()
+        {
+            if (Age >= 10)
+                WriteLine("I am flying");
+        }
+    }
+
+    public interface ILizzard : ICreature
+    {
+        void Crawl()
+        {
+            if (Age < 10)
+                WriteLine("I am crawling");
+        }
+    }
+
+    public class Organism { }
+
+    public class Dragon : Organism, IBird, ILizzard
+    {
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dragon { Age = 5};
+
+            if (d is IBird bird)
+                bird.Fly();
+
+            if (d is ILizzard lizzard)
+                lizzard.Crawl();
+        }
+    }
+}
