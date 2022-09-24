2fcea0b41d358dd52ae04af018e52f74c81c6218
adrian <adrian657984@gmail.com>
2022-09-25 03:33:57 +0800

    Proxy-Exercise

---
+++ Proxy/Exercise/Program.cs
@@ -0,0 +1,58 @@
+﻿ResponsiblePerson rPerson = new ResponsiblePerson(new Person { Age = 18 });
+Console.WriteLine(rPerson.Drink());
+Console.WriteLine(rPerson.Drive());
+
+public class Person
+{
+    public int Age { get; set; }
+
+    public string Drink()
+    {
+        return "drinking";
+    }
+
+    public string Drive()
+    {
+        return "driving";
+    }
+
+    public string DrinkAndDrive()
+    {
+        return "driving while drunk";
+    }
+}
+
+public class ResponsiblePerson
+{
+    private readonly Person _person;
+    public ResponsiblePerson(Person person)
+    {
+        // todo
+        _person = person;
+    }
+
+    public int Age 
+    { 
+        get { return _person.Age; } 
+        set { _person.Age = value; } 
+    }
+
+    public string Drink()
+    {
+        if (_person.Age >= 18)
+            return _person.Drink();
+        return "too young";
+    }
+
+    public string Drive()
+    {
+        if (_person.Age >= 16)
+            return _person.Drive();
+        return "too young";
+    }
+
+    public string DrinkAndDrive()
+    {
+        return "dead";
+    }
+}
